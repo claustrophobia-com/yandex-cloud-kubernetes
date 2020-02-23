1578345438f40ab343c6c86826f76e0cc70e1dfe
Daniel Chaplin <black.danek@gmail.com>
2020-02-23 21:10:05 +0300

    elasticsearch/logstash: add type and nginx filters

--- modules/elasticsearch/modules/logstash/main.tf
+++ modules/elasticsearch/modules/logstash/main.tf
@@ -44,6 +44,58 @@ locals {
       }
       EOF
   }
+  filters = {
+    type = <<-EOF
+    filter {
+      mutate {
+        add_field => {
+          "type" => "%%{[agent][type]}"
+        }
+      }
+    }
+    EOF
+    nginx = <<-EOF
+    filter {
+      if [event][module] == "nginx" {
+        if [fileset][name] == "access" {
+          grok {
+            match => { "message" => ["%%{IPORHOST:[nginx][access][remote_ip]} - %%{DATA:[nginx][access][user_name]} \[%%{HTTPDATE:[nginx][access][time]}\] \"%%{WORD:[nginx][access][method]} %%{DATA:[nginx][access][url]} HTTP/%%{NUMBER:[nginx][access][http_version]:float}\" %%{NUMBER:[nginx][access][response_code]:int} %%{NUMBER:[nginx][access][body_sent][bytes]:int} \"%%{DATA:[nginx][access][referrer]}\" \"%%{DATA:[nginx][access][agent]}\" %%{NUMBER:[nginx][access][request_length]:int} %%{NUMBER:[nginx][access][request_time]:float} \[%%{DATA:[nginx][access][proxy_upstream_name]}\] \[%%{DATA:[nginx][access][proxy_alternative_upstream_name]}\] %%{DATA:[nginx][access][upstream_addr]} %%{NUMBER:[nginx][access][upstream_response_length]:int} %%{NUMBER:[nginx][access][upstream_response_time]:float} %%{NUMBER:[nginx][access][upstream_status]:int} %%{DATA:[nginx][access][req_id]}"] }
+            remove_field => "message"
+          }
+          mutate {
+            add_field => { "read_timestamp" => "%%{@timestamp}" }
+          }
+          date {
+            match => [ "[nginx][access][time]", "dd/MMM/YYYY:H:m:s Z" ]
+            remove_field => "[nginx][access][time]"
+          }
+          useragent {
+            source => "[nginx][access][agent]"
+            target => "[nginx][access][user_agent]"
+            remove_field => "[nginx][access][agent]"
+          }
+          geoip {
+            source => "[nginx][access][remote_ip]"
+            target => "[nginx][access][geoip]"
+          }
+        }
+        else if [fileset][name] == "error" {
+          grok {
+            match => { "message" => ["%%{DATA:[nginx][error][time]} \[%%{DATA:[nginx][error][level]}\] %%{NUMBER:[nginx][error][pid]}#%%{NUMBER:[nginx][error][tid]}: (\*%%{NUMBER:[nginx][error][connection_id]} )?%%{GREEDYDATA:[nginx][error][message]}"] }
+            remove_field => "message"
+          }
+          mutate {
+            rename => { "@timestamp" => "read_timestamp" }
+          }
+          date {
+            match => [ "[nginx][error][time]", "YYYY/MM/dd H:m:s" ]
+            remove_field => "[nginx][error][time]"
+          }
+        }
+      }
+    }
+    EOF
+  }
   values = {
     nodeSelector = var.node_selector
     replicaCount = var.scale
@@ -54,7 +106,8 @@ locals {
       ports = local.service_ports
     }
     ports = local.ports
-    inputs = local.inputs
+    inputs = merge(local.inputs, { main = "" })
+    filters = local.filters
     outputs = {
       main = <<-EOF
       output {
@@ -76,7 +129,9 @@ resource "helm_release" "logstash" {
   name = "logstash"
   repository = data.helm_repository.stable.metadata[0].name
   chart = "logstash"
+  version = ""
   namespace = var.namespace
+  timeout = 600
 
   values = [yamlencode(local.values)]
 }
