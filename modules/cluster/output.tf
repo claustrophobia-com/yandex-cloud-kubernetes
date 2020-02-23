ef74b74b187803780f3a456e75c6fb9e67b908f4
Daniel Chaplin <black.danek@gmail.com>
2020-02-23 20:51:09 +0300

    cluster: add ca_certificate for output

--- modules/cluster/output.tf
+++ modules/cluster/output.tf
@@ -7,3 +7,6 @@ output "node_group_ids" {
 output "external_v4_endpoint" {
   value = yandex_kubernetes_cluster.cluster.master[0].external_v4_endpoint
 }
+output "ca_certificate" {
+  value = yandex_kubernetes_cluster.cluster.master[0].cluster_ca_certificate
+}
