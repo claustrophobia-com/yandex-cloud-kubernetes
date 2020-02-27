f6b5c2fde086b1f92ad995224309e799bf59b528
Daniel Chaplin <black.danek@gmail.com>
2020-02-28 02:47:23 +0300

    main: define type for input variables

--- variables.tf
+++ variables.tf
@@ -1,23 +1,66 @@
-variable "yandex_token" {}
-variable "yandex_cloud_id" {}
-variable "yandex_folder_id" {}
+variable "yandex_token" {
+  type = string
+}
+variable "yandex_cloud_id" {
+  type = string
+}
+variable "yandex_folder_id" {
+  type = string
+}
 variable "cluster_name" {
   type = string
 }
 variable "cluster_version" {
   type = string
+  default = "1.15"
 }
 variable "cluster_release_channel" {
   type = string
+  default = "STABLE"
 }
-variable "node_groups_scale" {}
-variable "admin_email" {}
-variable "cluster_domain" {}
-variable "admins" {}
+variable "node_groups_scale" {
+  default = {
+    service = {
+      fixed_scale = 3
+    }
+    nfs = {
+      fixed_scale = 1
+    }
+    web = {
+      auto_scale = {
+        max = 3
+        min = 3
+        initial = 3
+      }
+    }
+  }
+}
+variable "admin_email" {
+  type = string
+}
+variable "cluster_domain" {
+  type = string
+}
+variable "admins" {
+  type = map(object({
+    public_keys = list(string)
+  }))
+}
 variable "output_dir" {
+  type = string
   default = "output"
 }
 variable "secret_dir" {
+  type = string
   default = "secrets"
 }
-variable "service_email" {}
+variable "service_email" {
+  type = object({
+    from_address = string
+    host = string
+    port = string
+    user = string
+    password = string
+    use_tls = bool
+  })
+}
