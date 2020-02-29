bf96377f1692ba28e71b90790a88bbb2e3bd9289
Daniel Chaplin <black.danek@gmail.com>
2020-03-01 01:40:08 +0300

    admins: add variables type

--- modules/admins/variables.tf
+++ modules/admins/variables.tf
@@ -1,3 +1,11 @@
-variable "admins" {}
-variable "cluster_name" {}
-variable "cluster_endpoint" {}
+variable "admins" {
+  type = map(object({
+    public_keys = list(string)
+  }))
+}
+variable "cluster_name" {
+  type = string
+}
+variable "cluster_endpoint" {
+  type = string
+}
