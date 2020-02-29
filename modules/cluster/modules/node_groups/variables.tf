7313939f71100cd3ef7203cab4449d0da94ceac4
Daniel Chaplin <black.danek@gmail.com>
2020-03-01 01:39:51 +0300

    cluster: add variables type

--- modules/cluster/modules/node_groups/variables.tf
+++ modules/cluster/modules/node_groups/variables.tf
@@ -1,5 +1,33 @@
-variable "cluster_id" {}
-variable "kube_version" {}
-variable "location_subnets" {}
-variable "cluster_node_groups" {}
-variable "ssh_keys" {}
+variable "cluster_id" {
+  type = string
+}
+variable "kube_version" {
+  type = string
+  default = "1.15"
+}
+variable "location_subnets" {
+  type = list(object({
+    id = string
+    zone = string
+  }))
+}
+variable "cluster_node_groups" {
+  type = map(object({
+    name = string
+    cpu = number
+    memory = number
+    disk = object({
+      size = number
+      type = string
+    })
+    fixed_scale = list(number)
+    auto_scale = list(object({
+      max = number
+      min = number
+      initial = number
+    }))
+  }))
+}
+variable "ssh_keys" {
+  type = string
+}
--- modules/cluster/variables.tf
+++ modules/cluster/variables.tf
@@ -1,17 +1,54 @@
-variable "name" {}
-variable "public" {}
+variable "name" {
+  type = string
+}
+variable "public" {
+  type = bool
+  default = true
+}
 variable "region" {
+  type = string
   default = "ru-central1"
 }
-variable "kube_version" {}
-variable "release_channel" {}
-variable "vpc_id" {}
-variable "location_subnets" {}
+variable "kube_version" {
+  type = string
+  default = "1.15"
+}
+variable "release_channel" {
+  type = string
+  default = "STABLE"
+}
+variable "vpc_id" {
+  type = string
+}
+variable "location_subnets" {
+  type = list(object({
+    id = string
+    zone = string
+  }))
+}
 variable "cluster_service_account_id" {
   type = string
 }
 variable "node_service_account_id" {
   type = string
 }
-variable "cluster_node_groups" {}
-variable "ssh_keys" {}
+variable "cluster_node_groups" {
+  type = map(object({
+    name = string
+    cpu = number
+    memory = number
+    disk = object({
+      size = number
+      type = string
+    })
+    fixed_scale = list(number)
+    auto_scale = list(object({
+      max = number
+      min = number
+      initial = number
+    }))
+  }))
+}
+variable "ssh_keys" {
+  type = string
+}
