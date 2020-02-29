20dfdc51df1e8eee887666222f1b934a30cca406
Daniel Chaplin <black.danek@gmail.com>
2020-03-01 01:44:03 +0300

    vpc: add variables type

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -1,8 +1,12 @@
-variable "name" {}
+variable "name" {
+  type = string
+}
 variable "subnet" {
+  type = string
   default = "10.0.0.0/12"
 }
 variable "zones" {
+  type = list(string)
   default = [
     "ru-central1-a",
     "ru-central1-b",
