5a762c4addfa67e6059f9565e2ce3951d6b782c8
Daniel Chaplin <black.danek@gmail.com>
2020-03-01 01:41:43 +0300

    iam: add variables type

--- modules/iam/variables.tf
+++ modules/iam/variables.tf
@@ -1,3 +1,9 @@
-variable "cluster_folder_id" {}
-variable "cluster_service_account_name" {}
-variable "cluster_node_service_account_name" {}
+variable "cluster_folder_id" {
+  type = string
+}
+variable "cluster_service_account_name" {
+  type = string
+}
+variable "cluster_node_service_account_name" {
+  type = string
+}
