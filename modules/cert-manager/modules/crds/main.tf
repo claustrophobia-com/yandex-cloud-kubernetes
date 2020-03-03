d665b66fa9148ebab0f5d561bce32a2bb8bcb962
Daniel Chaplin <black.danek@gmail.com>
2020-03-04 01:33:32 +0300

    cert-manager/crds: upgrade to 0.13

--- modules/cert-manager/modules/crds/main.tf
+++ modules/cert-manager/modules/crds/main.tf
@@ -1,5 +1,5 @@
 data "http" "crds_manifest" {
-  url = "https://raw.githubusercontent.com/jetstack/cert-manager/release-0.12/deploy/manifests/00-crds.yaml"
+  url = "https://raw.githubusercontent.com/jetstack/cert-manager/release-0.13/deploy/manifests/00-crds.yaml"
 }
 
 locals {
